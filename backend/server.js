3dcadb38fa710b77302b27f76aeded874e542999
kaweesha weerasinghe <weerasinghamkh.23@uom.lk>
2026-09-10 23:21:22 +0530

    feat: add course management routes and controller

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
+const courseRoutes = require('./routes/courseRoutes');
 
 dotenv.config();
 connectDB();
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
+app.use('/api/courses', courseRoutes);
 
 const PORT = process.env.PORT || 5000;
 
